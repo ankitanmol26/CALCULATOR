60c7a87066b754075fbc26ecf4141045eb66e4ae
ankitanmol26 <ankitanmol26@gmail.com>
2026-05-31 23:56:17 +0530

    Save calculator changes

---
+++ script.js
@@ -0,0 +1,16 @@
+let input = document.getElementById('input');
+let buttons = document.querySelectorAll('button');
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        } else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    });
+});
